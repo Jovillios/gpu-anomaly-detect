d0137f43d6b7a56b7421169c7c00bd325073d719
jovillios <jules.decaestecker@ens-paris-saclay.fr>
2025-08-28 17:10:11 +0000

    add window size

--- rolling_mean.cu
+++ rolling_mean.cu
@@ -2,20 +2,28 @@
 #include <vector>
 
 // CUDA kernel: simple rolling mean with window=2 (toy example)
-__global__ void rolling_mean_kernel(const float* __restrict__ x, float* __restrict__ y, int N) {
-    int i = blockIdx.x * blockDim.x + threadIdx.x;
-    if (i < N - 1) {
-        y[i] = (x[i] + x[i+1]) / 2.0f;
+__global__ void rolling_mean_kernel(const float* __restrict__ x, float* __restrict__ y, int N, int W) {
+    int idx = blockIdx.x * blockDim.x + threadIdx.x;
+    if (idx >= N) return ;
+
+    int start = max(0, idx - W + 1);
+    int count = idx - start + 1;
+    float sum = 0.0;
+
+    for (int j=start; j <= idx; j++)
+    {
+        sum += x[j];
     }
+    y[idx] = sum / count;
 }
 
 // C++ wrapper
-torch::Tensor rolling_mean(torch::Tensor x) {
+torch::Tensor rolling_mean(torch::Tensor x, int W) {
     auto y = torch::zeros_like(x);
     int N = x.size(0);
     int threads = 256;
     int blocks = (N + threads - 1) / threads;
-    rolling_mean_kernel<<<blocks, threads>>>(x.data_ptr<float>(), y.data_ptr<float>(), N);
+    rolling_mean_kernel<<<blocks, threads>>>(x.data_ptr<float>(), y.data_ptr<float>(), N, W);
     return y;
 }
 
--- test.py
+++ test.py
@@ -2,6 +2,6 @@ import torch
 import rolling_mean_cuda
 
 x = torch.arange(10, dtype=torch.float32, device="cuda")
-y = rolling_mean_cuda.rolling_mean(x)
+y = rolling_mean_cuda.rolling_mean(x, 3)
 print("Input:", x)
 print("Rolling mean:", y)
